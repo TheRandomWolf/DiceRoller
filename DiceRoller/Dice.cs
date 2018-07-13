7f2421c36d70963c66378eb8267324ac3df207fa
Ori Munk <oriko1500@gmail.com>
2018-07-13 15:01:13 +0300

    First commit. I finished writing the initial code but haven't conducte any tests yet. TODO: Add README(DailyProgrammer Easy364)

---
+++ DiceRoller/Dice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+	public class Dice
+	{
+		protected static Random r;
+		public Dice(byte sides)
+		{
+			Sides = sides;
+			byte[] seed = new byte[1];
+			new RNGCryptoServiceProvider().GetBytes(seed);
+			r = new Random(seed[0]);
+		}
+		public int Sides { get; protected set; }
+		public int[] Roll(int times = 1) 
+		{
+			int[] results = new int[times];
+			for(int i = 0; i < results.Length; i++)
+			{
+				results[i] = r.Next(1, Sides);
+			}
+			return results;
+		}
+	}
+}
--- DiceRoller/Program.cs
+++ DiceRoller/Program.cs
@@ -8,8 +8,44 @@ namespace DiceRoller
 {
 	class Program
 	{
+		static bool firstTime = true;
 		static void Main(string[] args)
 		{
+			if (firstTime == true)
+			{
+				Console.WriteLine("Hello! Please input the amount of dice you want to roll and the dice's number of sides with the letter 'd' inbetween them like this: 3d5");
+				Console.WriteLine("the example above would roll 3 dice with 5 sides.");
+				Console.WriteLine("If you want to exit the program please type 'exit'");
+				firstTime = false;
+			}
+			string input = Console.ReadLine();
+			if (input.ToLower() == "exit")
+			{
+				Environment.Exit(0);
+			}
+			else
+			{
+				string[] diceInfo = input.ToLower().Split('d');
+				int times, sides;
+				int.TryParse(diceInfo[0], out times);
+				int.TryParse(diceInfo[1], out sides);
+				int[] rolls =GetDiceRolls(sides, times);
+				string output = FormatResults(rolls);
+				Console.WriteLine(output);
+			}
+		}
+		static int[] GetDiceRolls(int sides, int times)
+		{
+			Dice d = new DiceRoller.Dice(sides);
+			int[] rolls = d.Roll(times);
+			return rolls;
+		}
+		static string FormatResults(int[] results)
+		{
+			int total = results.Sum();
+			StringBuilder sb = new StringBuilder("{0} : ", total);
+			foreach (int roll in results) sb.Append(roll + ", ");
+			return sb.ToString();
 		}
 	}
 }
